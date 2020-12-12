d33e7cddd6d08e238ac378850bb9edfcdbdc6ca9
Jason Williams <jwilliamstap@gmail.com>
2020-12-12 12:03:16 -0500

    Jasons commit

--- src/Groceries.js
+++ src/Groceries.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
-import connect from 'react-redux'
+import { connect } from 'react-redux'
 
 //21.54
 const Groceries = ({ groceries }) => {
     return (
         <ul>
-            {groceries.map((grocery) => {
-                return <li key={grocery.id}>{grocery.name}</li>
-            })}
+            {
+                groceries.map((grocery) => {
+                    return (
+                        <li key={grocery.id}>
+                            {grocery.name}
+                        </li>
+                    )
+                })
+            }
         </ul>
     )
 }
 
-export default connect(Groceries)
+const mapStateToProps = ({ groceries })=> {
+    return {
+        groceries
+    }
+}
+
+export default connect(mapStateToProps)(Groceries)
--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import connect from 'react-redux'
+import Groceries from './Groceries'
+import axios from 'axios'
 
 const store = createStore(
     (state = { loading: true, groceries: [] }, action) => {
+        if(action.type === 'LOADED'){
+            state = {...state, loading: false };
+        }
+        else if(action.type === 'LOAD_GROCERIES'){
+            state = {...state, groceries: action.groceries };
+        }
         return state
     }
 )
+
+const LOADED = 'LOADED';
+const LOAD_GROCERIES = 'LOAD_GROCERIES';
+
+const loaded = () => {
+    return {
+        type: LOADED
+    };
+}
+
+const loadGroceries = (groceries) => {
+    return {
+        type: LOAD_GROCERIES,
+        groceries
+    }
+}
 
 class _App extends Component {
     render() {
@@ -21,29 +45,40 @@ class _App extends Component {
     }
 }
 
-const mapStateToProps = ({ loading }) => {
-    return {
-        loading,
-    }
-}
+// const mapStateToProps = ({ loading }) => {
+//     return {
+//         loading,
+//     }
+// }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        load: async () => {
-            const groceries = (await axios.get('/api/groceries')).data
-            dispatch({
-                type: 'LOAD_GROCERIES',
-                groceries,
-            })
-            dispatch({
-                type: 'LOADED',
-                groceries,
-            })
-        },
-    }
-}
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         load: async () => {
+//             const groceries = (await axios.get('/api/groceries')).data
+//             dispatch({
+//                 type: 'LOAD_GROCERIES',
+//                 groceries,
+//             })
+//             dispatch({
+//                 type: 'LOADED',
+//                 groceries,
+//             })
+//         },
+//     }
+// }
 
-const App = connect(mapStateToProps, mapDispatchToProps)(_App) // mapDispatchToProps
+const App = connect(
+    state=> state,
+    (dispatch)=> {
+        return {
+            load: async()=> {
+                const groceries = (await axios.get('/api/groceries')).data;
+                dispatch(loaded());
+                dispatch(loadGroceries(groceries));
+            }
+        }
+    }
+)(_App);
 
 render(
     <Provider store={store}>
